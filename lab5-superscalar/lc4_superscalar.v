71a0cfbbc891474dfde12f58b908cd19025876c2
Lihong Zhao <lihzhao@big03.seas.upenn.edu>
2023-04-05 22:22:38 -0400

    complete Decode

--- lab5-superscalar/lc4_superscalar.v
+++ lab5-superscalar/lc4_superscalar.v
@@ -89,3 +89,166 @@ module lc4_processor(input wire         clk,             // main clock
     Nbit_reg #(2, 2'h2) FD_stall_B (.in(2'h0), .out(D_IR_stall_B), .clk(clk), .we(F_we), .gwe(gwe), .rst(rst));
 
 
+/**********************************************DECODE STAGE**********************************************/
+
+/****Stall Logic****/
+    wire [2:0] D_stall_r1_A;
+    wire [2:0] D_stall_r2_A;
+    wire [2:0] D_stall_rd_A;
+    wire D_stall_r1re_A;
+    wire D_stall_r2re_A;
+    wire D_stall_is_store_A;
+    wire D_stall_is_branch_A;
+    wire D_stall_regfile_we_A;
+
+
+    wire [2:0] D_stall_r1_B;
+    wire [2:0] D_stall_r2_B;
+    wire [2:0] D_stall_rd_B;
+    wire D_stall_r1re_B;
+    wire D_stall_r2re_B;
+    wire D_stall_is_store_B;
+    wire D_stall_is_branch_B;
+    wire D_stall_regfile_we_B;
+
+    //Use parallel decoder to detect stalls because other decoder will be flushed with NOP when a stall is needed. Can't decode stall logic and flush the same decoder in 1 cycle
+    lc4_decoder stall_decoder_A(.insn(D_IR_insn_A), .r1sel(D_stall_r1_A), .r1re(D_stall_r1re_A), .r2sel(D_stall_r2_A), .r2re(D_stall_r2re_A), .wsel(D_stall_rd_A), 
+    .regfile_we(D_stall_regfile_we_A), .nzp_we(), .select_pc_plus_one(), .is_load(), .is_store(D_stall_is_store_A), .is_branch(D_stall_is_branch_A), .is_control_insn());
+
+    lc4_decoder stall_decoder_B(.insn(D_IR_insn_B), .r1sel(D_stall_r1_B), .r1re(D_stall_r1re_B), .r2sel(D_stall_r2_B), .r2re(D_stall_r2re_B), .wsel(D_stall_rd_B),
+    .regfile_we(D_stall_regfile_we_B), .nzp_we(), .select_pc_plus_one(), .is_load(), .is_store(D_stall_is_store_B), .is_branch(D_stall_is_branch_B), .is_control_insn());
+
+    //ONLY SUPPORTS SUPERSCALAR STALL (EXCLUDING MEMORY)
+    assign stall_A = 2'h0;
+    assign stall_B =    (   ((D_stall_rd_A == D_stall_r1_B) & D_stall_r1re_B)       | 
+                            ((D_stall_rd_A == D_stall_r2_B) & D_stall_r2re_B)   )   &
+                            D_stall_regfile_we_A ? 2'h1 : 2'h0; // check the dependencies between A and B
+
+    wire [1:0] D_stall_A = D_IR_stall_A;
+    wire [1:0] D_stall_B = (stall_B != 3'h0) ? stall_B : D_IR_stall_B;
+
+    //decoder A outputs
+    wire [2:0] D_r1_A;
+    wire [2:0] D_r2_A;
+    wire [2:0] D_rd_A;
+    wire D_r1re_A;
+    wire D_r2re_A;
+    wire D_select_pc_plus_one_A;
+    wire D_is_load_A;
+    wire D_is_store_A;
+    wire D_is_branch_A;
+    wire D_is_control_insn_A;
+    wire D_regfile_we_A;
+    wire D_nzp_we_A;
+
+    //decoder B outputs
+    wire [2:0] D_r1_B;
+    wire [2:0] D_r2_B;
+    wire [2:0] D_rd_B;
+    wire D_r1re_B;
+    wire D_r2re_B;
+    wire D_select_pc_plus_one_B;
+    wire D_is_load_B;
+    wire D_is_store_B;
+    wire D_is_branch_B;
+    wire D_is_control_insn_B;
+    wire D_regfile_we_B;
+    wire D_nzp_we_B;
+
+    wire [15:0] D_insn_A = D_IR_insn_A; //change to support load-to-use stall and flush on branch
+    wire [15:0] D_insn_B = (stall_B == 2'h1) ? 16'h0 : D_IR_insn_B; //change to support load-to-use stall and flush on branch
+
+    //decoder A
+    lc4_decoder decoder_A (.insn(D_insn_A), .r1sel(D_r1_A), .r1re(D_r1re_A), .r2sel(D_r2_A), .r2re(D_r2re_A), .wsel(D_rd_A), .regfile_we(D_regfile_we_A), .nzp_we(D_nzp_we_A), .select_pc_plus_one(D_select_pc_plus_one_A),
+                            .is_load(D_is_load_A), .is_store(D_is_store_A), .is_branch(D_is_branch_A), .is_control_insn(D_is_control_insn_A));
+
+    //decoder B
+    lc4_decoder decoder_B (.insn(D_insn_B), .r1sel(D_r1_B), .r1re(D_r1re_B), .r2sel(D_r2_B), .r2re(D_r2re_B), .wsel(D_rd_B), .regfile_we(D_regfile_we_B), .nzp_we(D_nzp_we_B), .select_pc_plus_one(D_select_pc_plus_one_B),
+                            .is_load(D_is_load_B), .is_store(D_is_store_B), .is_branch(D_is_branch_B), .is_control_insn(D_is_control_insn_B));
+
+    wire [15:0] D_r1_data_A, D_r2_data_A, D_r1_data_B, D_r2_data_B; //regfile output wires
+    lc4_regfile_ss regfile(.i_rs_A(D_r1_A), .i_rt_A(D_r2_A), .i_rd_A(W_IR_rd_A), .i_rd_we_A(W_IR_regfile_we_A), .i_wdata_A(W_rd_data_A), .o_rs_data_A(D_r1_data_A), .o_rt_data_A(D_r2_data_A),
+                            .i_rs_B(D_r1_B), .i_rt_B(D_r2_B), .i_rd_B(W_IR_rd_B), .i_rd_we_B(W_IR_regfile_we_B), .i_wdata_B(W_rd_data_B), .o_rs_data_B(D_r1_data_B), .o_rt_data_B(D_r2_data_B),
+                            .clk(clk) , .gwe(gwe), .rst(rst)); //2-pipe regfile
+
+/****Decode-Execute Intermediate Register****/
+
+    //DX_reg A output wires
+    wire [15:0] X_IR_r1_data_A;
+    wire [15:0] X_IR_r2_data_A;
+    wire [2:0] X_IR_r1_A;
+    wire [2:0] X_IR_r2_A;
+    wire [2:0] X_IR_rd_A;
+    wire X_IR_r1re_A;
+    wire X_IR_r2re_A;
+    wire X_IR_select_pc_plus_one_A;
+    wire X_IR_is_load_A;
+    wire X_IR_is_store_A;
+    wire X_IR_is_branch_A;
+    wire X_IR_is_control_insn_A;
+    wire X_IR_regfile_we_A;
+    wire X_IR_nzp_we_A;
+    wire [1:0] X_IR_stall_A;
+    //End new outputs
+    wire [15:0] X_IR_pc_A;
+    wire [15:0] X_IR_insn_A;
+
+    //DX_reg B output wires
+    wire [15:0] X_IR_r1_data_B;
+    wire [15:0] X_IR_r2_data_B;
+    wire [2:0] X_IR_r1_B;
+    wire [2:0] X_IR_r2_B;
+    wire [2:0] X_IR_rd_B;
+    wire X_IR_r1re_B;
+    wire X_IR_r2re_B;
+    wire X_IR_select_pc_plus_one_B;
+    wire X_IR_is_load_B;
+    wire X_IR_is_store_B;
+    wire X_IR_is_branch_B;
+    wire X_IR_is_control_insn_B;
+    wire X_IR_regfile_we_B;
+    wire X_IR_nzp_we_B;
+    wire [1:0] X_IR_stall_B;
+    //End new outputs
+    wire [15:0] X_IR_pc_B;
+    wire [15:0] X_IR_insn_B;
+
+    //DX_reg A registers
+    Nbit_reg #(16, 16'h0) DX_r1_data_A (.in(D_r1_data_A), .out(X_IR_r1_data_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'h0) DX_r2_data_A (.in(D_r2_data_A), .out(X_IR_r2_data_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(3, 3'h0) DX_r1_A (.in(D_r1_A), .out(X_IR_r1_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(3, 3'h0) DX_r2_A (.in(D_r2_A), .out(X_IR_r2_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(3, 3'h0) DX_rd_A (.in(D_rd_A), .out(X_IR_rd_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_r1re_A (.in(D_r1re_A), .out(X_IR_r1re_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_r2re_A (.in(D_r2re_A), .out(X_IR_r2re_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_select_pc_plus_one_A (.in(D_select_pc_plus_one_A), .out(X_IR_select_pc_plus_one_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_is_load_A (.in(D_is_load_A), .out(X_IR_is_load_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_is_store_A (.in(D_is_store_A), .out(X_IR_is_store_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_is_branch_A (.in(D_is_branch_A), .out(X_IR_is_branch_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_is_control_insn_A (.in(D_is_control_insn_A), .out(X_IR_is_control_insn_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_regfile_we_A (.in(D_regfile_we_A), .out(X_IR_regfile_we_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_nzp_we_A (.in(D_nzp_we_A), .out(X_IR_nzp_we_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    //End new registers
+    Nbit_reg #(16, 16'h0) DX_pc_A (.in(D_IR_pc_A), .out(X_IR_pc_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'h0) DX_insn_A (.in(D_insn_A), .out(X_IR_insn_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(2, 2'h2) DX_stall_A (.in(D_stall_A), .out(X_IR_stall_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+
+    //DX_reg B registers
+    Nbit_reg #(16, 16'h0) DX_r1_data_B (.in(D_r1_data_B), .out(X_IR_r1_data_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'h0) DX_r2_data_B (.in(D_r2_data_B), .out(X_IR_r2_data_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(3, 3'h0) DX_r1_B (.in(D_r1_B), .out(X_IR_r1_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(3, 3'h0) DX_r2_B (.in(D_r2_B), .out(X_IR_r2_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(3, 3'h0) DX_rd_B (.in(D_rd_B), .out(X_IR_rd_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_r1re_B (.in(D_r1re_B), .out(X_IR_r1re_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_r2re_B (.in(D_r2re_B), .out(X_IR_r2re_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_select_pc_plus_one_B (.in(D_select_pc_plus_one_B), .out(X_IR_select_pc_plus_one_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_is_load_B (.in(D_is_load_B), .out(X_IR_is_load_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_is_store_B (.in(D_is_store_B), .out(X_IR_is_store_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_is_branch_B (.in(D_is_branch_B), .out(X_IR_is_branch_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_is_control_insn_B (.in(D_is_control_insn_B), .out(X_IR_is_control_insn_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_regfile_we_B (.in(D_regfile_we_B), .out(X_IR_regfile_we_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(1, 1'h0) DX_nzp_we_B (.in(D_nzp_we_B), .out(X_IR_nzp_we_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    //End new registers
+    Nbit_reg #(16, 16'h0) DX_pc_B (.in(D_IR_pc_B), .out(X_IR_pc_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'h0) DX_insn_B (.in(D_insn_B), .out(X_IR_insn_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(2, 2'h2) DX_stall_B (.in(D_stall_B), .out(X_IR_stall_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
